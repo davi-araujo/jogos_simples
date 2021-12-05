bb38f9142f6feedd0c9bdaed103ffb67b173c457
Davi Matias Araújo <55163212+davi-araujo@users.noreply.github.com>
2021-12-05 00:21:15 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -25,7 +25,6 @@ def jogo():
 
     while not acertou and not enforcou:
         acertou_letra = False
-        desenha_forca(len(letras_erradas))
         chute = input("Qual a letra?")
         chute = chute.strip()
 
@@ -49,88 +48,16 @@ def jogo():
         for letras in letras_erradas:
             print(letras, end=" ")
         print("")
-        print(f"Tentativas restantes: {7 - len(letras_erradas)}")
+        print(f"Tentativas restantes: {6 - len(letras_erradas)}")
 
         if list(palavra_secreta) == letras_acertadas:
             acertou = True
-            imprime_mensagem_vencedor()
+            print("Voce escapou da forca")
         if len(letras_erradas) == 7:
             enforcou = True
-            imprime_mensagem_perdedor(palavra_secreta)
+            print("Voce morreu enforcado")
 
     print("Fim do jogo")
-
-
-def imprime_mensagem_perdedor(palavra_secreta):
-    print("Puxa, você foi enforcado!")
-    print("A palavra era {}".format(palavra_secreta))
-    print("       {}           {}")
-    print("         \  _---_  /")
-    print("          \/     \/")
-    print("           |() ()|")
-    print("            \ + /")
-    print("           / HHH  \\")
-    print("          /  \_/   \\")
-    print("        {}          {}")
-
-
-def imprime_mensagem_vencedor():
-    print("Parabéns, você ganhou!")
-    print("       ___________      ")
-    print("      '._==_==_=_.'     ")
-    print("      .-\\:      /-.    ")
-    print("     | (|:.     |) |    ")
-    print("      '-|:.     |-'     ")
-    print("        \\::.    /      ")
-    print("         '::. .'        ")
-    print("           ) (          ")
-    print("         _.' '._        ")
-    print("        '-------'       ")
-
-
-def desenha_forca(erros):
-    print("  _______     ")
-    print(" |/      |    ")
-
-    if erros == 1:
-        print(" |      (_)   ")
-        print(" |            ")
-        print(" |            ")
-        print(" |            ")
-
-    if erros == 2:
-        print(" |      (_)   ")
-        print(" |      \     ")
-        print(" |            ")
-        print(" |            ")
-
-    if erros == 3:
-        print(" |      (_)   ")
-        print(" |      \|    ")
-        print(" |            ")
-        print(" |            ")
-
-    if erros == 4:
-        print(" |      (_)   ")
-        print(" |      \|/   ")
-        print(" |            ")
-        print(" |            ")
-
-    if erros == 5:
-        print(" |      (_)   ")
-        print(" |      \|/   ")
-        print(" |       |    ")
-        print(" |            ")
-
-    if erros == 6:
-        print(" |      (_)   ")
-        print(" |      \|/   ")
-        print(" |       |    ")
-        print(" |      /     ")
-
-    print(" |            ")
-    print("_|___         ")
-    print()
 
 
 if __name__ == "__main__":
