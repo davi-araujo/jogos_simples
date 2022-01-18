8ed42dcb06ee07b2cd32763ba594091cad2fdead
Davi Matias Araújo <55163212+davi-araujo@users.noreply.github.com>
2022-01-18 13:04:29 -0300

    Update jogos.py

--- jogos.py
+++ jogos.py
@@ -1,13 +1,25 @@
 import adivinhacao
 import forca
+import jogo_da_velha
+import blackjack
+import os
+import time
 
 
-def escolhe_jogo():
+def clear():
+    os.system('cls')
+
+
+def printa_menu():
+    clear()
     print("***************************")
     print("Bem vindo ao menu de jogos!")
     print("***************************")
+    print("(1) Jogo da forca", "(2) Jogo de adivinhação", "(3) Jogo da velha", "(4) BlackJack", sep="\n")
 
-    print("(1) Jogo da forca", "(2) Jogo de adivinhação", sep="\n")
+
+def escolhe_jogo():
+    printa_menu()
 
     jogo = int(input("Escolha seu jogo: "))
 
@@ -15,6 +27,14 @@ def escolhe_jogo():
         forca.jogo()
     elif jogo == 2:
         adivinhacao.jogo()
+    elif jogo == 3:
+        jogo_da_velha.jogo()
+    elif jogo == 4:
+        blackjack.jogo()
+    else:
+        clear()
+        print("Escolha inválida. Por favor, escolha uma opção válida.")
+        time.sleep(3)
 
 
 if __name__ == "__main__":
